17bb2817eee857f7f5001e249803e2492643f088
Prasad-12345 <prasadsomvanshi471@gmail.com>
2025-08-27 11:40:30 +0530

    small changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = location.hostname == "localhost" ? "http://localhost:7777" : "/api"
+export const BASE_URL = location.hostname == "localhost" ? "http://localhost:7777" : "https://dev-tinder-backend-0x2q.onrender.com"
